fc6cbce227200359a797c7df93cf76d3de812dcd
Fabio Greotti <f.greotti@redkoibox.it>
2018-04-20 16:10:37 +0200

    + Added new prebuilt libs. + Added support for automatic compile (CircleCI).

--- build-linux.sh
+++ build-linux.sh
@@ -2,4 +2,8 @@
 mkdir build
 cd build
 export LUA_DIR=/usr/local
-cmake ../ -DLIBBSON_DIR=/usr/local -DLIBMONGOC_DIR=/usr/local
+cmake -G "Unix Makefiles" -DLIBBSON_DIR=/usr/local -DLIBMONGOC_DIR=/usr/local -DCMAKE_INSTALL_PREFIX=/tmp/builds -DMICROHELIX_INSTALL_DIR=/usr/local ../
+make
+make install
+mkdir compiled
+tar czvf /usr/local/microhelix ./compiled/microhelix.tar.gz
